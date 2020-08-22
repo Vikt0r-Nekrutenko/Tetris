715f2dc4548a96f6a1c033feecc7a9ca26845cbb
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2020-08-22 23:32:55 +0300

    game app class added

--- ConsolEngine/main.cpp
+++ ConsolEngine/main.cpp
@@ -1,9 +1,40 @@
 // Copyright 2009-2014 Blam Games, Inc. All Rights Reserved.
 
 #include "TestApp.h"
+#include <conio.h>
+
+constexpr int WINDOW_W = 25;
+constexpr int WINDOW_H = 25;
+
+class GameApp : public BaseApp
+{
+public:
+	GameApp() : BaseApp(WINDOW_W, WINDOW_H)
+	{
+		CONSOLE_SCREEN_BUFFER_INFOEX csbiex;
+		csbiex.cbSize = sizeof(CONSOLE_SCREEN_BUFFER_INFOEX);
+		GetConsoleScreenBufferInfoEx(GetStdHandle(STD_OUTPUT_HANDLE), &csbiex);
+		csbiex.dwSize.X = csbiex.dwMaximumWindowSize.X = WINDOW_W;
+		csbiex.dwSize.Y = csbiex.dwMaximumWindowSize.Y = WINDOW_H;
+		SetConsoleScreenBufferInfoEx(GetStdHandle(STD_OUTPUT_HANDLE), &csbiex);
+	}
+
+	void UpdateF(float deltaTime) final
+	{
+
+	}
+
+	void KeyPressed(int btnCode) final
+	{
+		if (btnCode == 224)
+		{
+			btnCode = getch();
+		}
+	}
+};
 
 void main ()
 {
-	TestApp app;
+	GameApp app;
 	app.Run();
 }
